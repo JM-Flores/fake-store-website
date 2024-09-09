429077195a0141810d947da978cc089b7e985051
JM-Flores <jmiguelflores19@gmail.com>
2024-09-09 22:52:53 +0800

    Fix search bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,23 +5,36 @@ import {
   InputGroup,
   InputRightElement,
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import useUpdateURLQuery from "../hooks/useUpdateURLQuery";
+import useProductQueryStore from "../store/productQueryStore";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
 
-  const { updateSearchParams } = useUpdateURLQuery();
+  const storeSearchText = useProductQueryStore(
+    (s) => s.productQuery.searchText
+  );
+  useEffect(() => {
+    if (ref.current && !storeSearchText) {
+      ref.current.value = "";
+    }
+  }, [storeSearchText]);
 
+  const { updateSearchParams } = useUpdateURLQuery();
   const updateSearchText = (searchText: string) => {
     updateSearchParams({ searchText: searchText });
   };
+
+  const navigate = useNavigate();
 
   return (
     <>
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          navigate("/products");
           if (ref.current) updateSearchText(ref.current.value);
         }}
       >
