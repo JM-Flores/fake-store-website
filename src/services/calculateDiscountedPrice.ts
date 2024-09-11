ade6826b9891883548a188f20582f8de12f17320
JM-Flores <jmiguelflores19@gmail.com>
2024-09-11 20:19:57 +0800

    Fixed price bug

--- src/services/calculateDiscountedPrice.ts
+++ src/services/calculateDiscountedPrice.ts
@@ -1,6 +1,8 @@
 const calculateDiscountPrice = (price?: number, discountPercent?: number) => {
     if (!price || !discountPercent) return 0;
-    return parseInt((price * (100 - discountPercent)/ 100).toFixed(2));
+    const result = parseFloat((price * (100 - discountPercent)/ 100).toFixed(2));
+    console.log (price + ' x ' + discountPercent + '% = ' + result);
+    return result;
 }
 
 export default calculateDiscountPrice;
