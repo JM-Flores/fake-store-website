b73c8473f4c129d85b69215ebb490832385180f6
JM-Flores <jmiguelflores19@gmail.com>
2024-09-11 20:20:43 +0800

    Remove console log

--- src/services/calculateDiscountedPrice.ts
+++ src/services/calculateDiscountedPrice.ts
@@ -1,7 +1,6 @@
 const calculateDiscountPrice = (price?: number, discountPercent?: number) => {
     if (!price || !discountPercent) return 0;
     const result = parseFloat((price * (100 - discountPercent)/ 100).toFixed(2));
-    console.log (price + ' x ' + discountPercent + '% = ' + result);
     return result;
 }
 
