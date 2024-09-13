80f83cbb268af563630b875d1e7ddf293092bb8c
JM-Flores <jmiguelflores19@gmail.com>
2024-09-13 23:01:49 +0800

    Add product listing category title

--- src/pages/ProductsListingPage.tsx
+++ src/pages/ProductsListingPage.tsx
@@ -1,12 +1,25 @@
-import { Grid } from "@chakra-ui/react";
+import { Box, Grid, Heading } from "@chakra-ui/react";
 import SideBar from "../components/SideBar";
 import ProductGrid from "../components/ProductGrid";
+import useProductQueryStore from "../store/productQueryStore";
+import useCategories from "../hooks/useCategories";
 
 const ProductsListingPage = () => {
+  const categorySlug = useProductQueryStore((s) => s.productQuery.category);
+  const { data: categories, error, isLoading } = useCategories();
+  const category = categories?.find((c) => c.slug === categorySlug)?.name;
+
   return (
     <Grid templateColumns={"220px 1fr"}>
       <SideBar />
-      <ProductGrid />
+      <Box>
+        {category && (
+          <Heading mt={5} ml={5} fontSize={25}>
+            {category}
+          </Heading>
+        )}
+        <ProductGrid />
+      </Box>
     </Grid>
   );
 };
